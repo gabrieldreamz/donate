39a271480eb5e819473b486754e2cb272c00f44a
Dream <drealmlex7@gmail.com>
2024-01-03 00:42:56 -0800

    Make some change to the home component and also added the aticle slug

--- components/blog/slug-articles/Description.tsx
+++ components/blog/slug-articles/Description.tsx
@@ -1,9 +1,9 @@
 export default function Description({ ArrText }: { ArrText: string[] }) {
   return (
     <div>
-      {ArrText.map((text) => (
+      {/* {ArrText.map((text) => (
         <p>{text}</p>
-      ))}
+      ))} */}
     </div>
   );
 }
