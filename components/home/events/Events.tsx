077217f2efbf2865850fc6c8dd8b82b0ab285197
Dream <drealmlex7@gmail.com>
2024-01-06 23:38:48 -0800

    OMO

--- components/home/events/Events.tsx
+++ components/home/events/Events.tsx
@@ -53,7 +53,7 @@ export default function Events() {
 
   return (
     <CenterCard>
-      <section className="px-3 sm:px-10 my-32 lg:my-44" id="featured">
+      <section className="px-5 sm:px-10 my-32 lg:my-44" id="featured">
         <div className="flex flex-col sm:flex-row sm:items-center gap-5 sm:justify-between">
           <h1 className="text-3xl text-dark text-center">
             Make someones{" "}
