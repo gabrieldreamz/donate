3041b29ce1013bed0096f3e280b2ed3920d392ff
Dream <drealmlex7@gmail.com>
2024-01-15 21:47:33 -0800

    AmorDivina

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,25 +1,25 @@
-// import { EmailTemplate } from "@components/mails/ResendEmailTemplate";
-// import { Resend } from "resend";
+import { EmailTemplate } from "@components/mails/ResendEmailTemplate";
+import { Resend } from "resend";
 
 // const resend = new Resend(process.env.RESEND_API_KEY);
 
-// export async function POST(req) {
-//   const { email } = await req.json();
+export async function POST(req) {
+  const { email } = await req.json();
 
-//   if (!email) throw new Error("Invalid email");
+  if (!email) throw new Error("Invalid email");
 
-//   try {
-//     const data = await resend.emails.send({
-//       from: "Acme <info@kindripple.com>",
-//       to: [email],
-//       subject: "the",
-//       react: EmailTemplate({ firstName: "John" }),
-//     });
+  try {
+    // const data = await resend.emails.send({
+    //   from: "Acme <info@kindripple.com>",
+    //   to: [email],
+    //   subject: "the",
+    //   react: EmailTemplate({ firstName: "John" }),
+    // });
 
-//     return new Response(JSON.stringify(data), { status: 200 });
-//   } catch (error) {
-//     return new Response(JSON.stringify({ error: error.message }), {
-//       status: 401,
-//     });
-//   }
-// }
+    return new Response({ status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 401,
+    });
+  }
+}
